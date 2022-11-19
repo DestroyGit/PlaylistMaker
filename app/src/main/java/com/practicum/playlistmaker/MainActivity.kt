a679a725e56aa4a926cf02f904965f0441a51be4
DestroyGit <estonmax@gmail.com>
2022-11-19 12:09:16 +0300

    Выполнение домашнего задания №2 1. Добавил обработку нажатия кнопок в activity_main.xml 2. Добавил Toast к кнопкам для проверки работоспособности

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.btn_search)
+        val buttonMedia = findViewById<Button>(R.id.btn_media)
+        val buttonSettings = findViewById<Button>(R.id.btn_settings)
+
+        buttonSearch.setOnClickListener {
+            Toast.makeText(this, "Нажали кнопку Поиск", Toast.LENGTH_SHORT).show()
+        }
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this, "Нажали кнопку Медиатека", Toast.LENGTH_SHORT).show()
+        }
+        buttonSettings.setOnClickListener {
+            Toast.makeText(this, "Нажали кнопку Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
