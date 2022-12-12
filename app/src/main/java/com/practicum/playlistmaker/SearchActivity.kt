231f0aa33cf499bdb24c795409cf86b897bcfbab
DestroyGit <estonmax@gmail.com>
2022-12-12 18:23:18 +0300

    1) Выполнение ДЗ №2: - сделал верстку для меню Поиска. Для темной темы тоже - добавил логику в работу EditText 2) Добавил стили для шапки не главных активити: Настройки и Поиск 3) Начал исправление dimens.xml в читаемый и понятный вид

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,47 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val textSearch = findViewById<EditText>(R.id.inputSearch)
+        val btnClearText = findViewById<ImageView>(R.id.clearTextSearch)
+
+        btnClearText.setOnClickListener(){
+            textSearch.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(textSearch.windowToken, 0)
+        }
+
+        val searchTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                btnClearText.visibility = visibilityView(p0)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        }
+
+        textSearch.addTextChangedListener(searchTextWatcher)
+    }
+
+    fun visibilityView(s: CharSequence?): Int{
+        return if (s.isNullOrEmpty()){
+            View.GONE
+        } else{
+            View.VISIBLE
+        }
     }
 }
